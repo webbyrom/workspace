9844632df549899ddb58162951ffa4e1a980231a
web-byrom <contact@web-byrom.com>
2019-08-27 21:40:23 +0200

    création effet sur h1

---
+++ js/tittle.js
@@ -0,0 +1,11 @@
+$(window).ready(function() {
+	$("h1").ready(function(){
+		$("h1").animate({
+			left: '0',
+			right: '0',
+			height: '85px',
+			top: '+=20px',
+			opacity: '1'
+		},4000);
+	});
+});	
