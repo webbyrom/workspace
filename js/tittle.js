51840680953ec9d5ff9a20e80e41e7c568f74c2f
web-byrom <contact@web-byrom.com>
2019-08-29 23:45:18 +0200

    modif js du tittle  + css du h1

--- js/tittle.js
+++ js/tittle.js
@@ -1,4 +1,65 @@
-$(window).ready(function() {
+//function(title) {
+	//recuperer tailel ecran dans variable 
+	//si taille = 960px alors h1 font-size =30px et top +=10px
+//}
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+  	$(window).ready(function() {
+		$("h1").ready(function(){
+			$("h1").animate({
+				left: '0',
+				right: '0',
+				top: '-=77px',
+				opacity: '0.5',
+				opacity: '1',
+				fontSize:'-=10px',
+				transitionDuration:'5s'
+				},4000);
+			$("h1").css({
+				color: 'purple'
+		});		
+	});
+});	
+    //document.body.style.backgroundColor = "yellow";//
+  } else {
+		$(window).ready(function() {
+				$("h1").ready(function(){
+					$("h1").animate({
+						left: '0',
+						right: '0',
+						top: '+=12px',
+						opacity: '0.5',
+						opacity: '1',
+						fontSize:'+=30px',
+						transitionDuration:'5s'
+						},4000);
+					$("h1").css({
+						color: 'purple'
+
+				});		
+			});
+		});
+   //document.body.style.backgroundColor = "pink";//
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+
+
+
+
+
+
+
+/*animation titre fonctionnelle mise en commentaire pour essais******/
+/*$(window).ready(function() {
 	$("h1").ready(function(){
 		$("h1").animate({
 			left: '0',
@@ -11,4 +72,4 @@ $(window).ready(function() {
 			color: 'purple'
 		});		
 	});
-});	
+});	*/
